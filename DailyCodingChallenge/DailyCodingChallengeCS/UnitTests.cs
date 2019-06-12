5b2202f771295127cefaf4f6f8d229cd0eb01d58
JasperDre <jasperdre@gmail.com>
2019-06-12 13:15:22 +0200

    Add new tests

--- DailyCodingChallenge/DailyCodingChallengeCS/UnitTests.cs
+++ DailyCodingChallenge/DailyCodingChallengeCS/UnitTests.cs
@@ -184,7 +184,7 @@ namespace DailyCodingChallengeCS
                 ways[0] = w;
             }
 
-            output =  ways[0];
+            output = ways[0];
 
             Assert.AreEqual(output, expected);
         }
@@ -193,7 +193,7 @@ namespace DailyCodingChallengeCS
         [TestMethod]
         public void TestMethod6()
         {
-            int[] input = { 1, 3, 6, 11};
+            int[] input = { 1, 3, 6, 11 };
             int output = 0;
             int expected = 12;
 
@@ -242,6 +242,24 @@ namespace DailyCodingChallengeCS
             {
                 result = true;
             }
+
+            Assert.IsTrue(result);
+        }
+
+        // Given an N by K matrix where the nth row and kth column represents the cost to build the nth house with kth color, return the minimum cost which achieves this goal.
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int[,] input = new int[,] { { 1, 2, 3, 4 }, { 1, 2, 1, 0 }, { 6, 1, 1, 5 } };
+
+            Assert.IsTrue(result);
+        }
+
+        // Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int[,] input = new int[,] { { 30, 75 }, { 0, 50 }, { 60, 150 } };
 
             Assert.IsTrue(result);
         }
